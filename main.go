5fec195bc457e758ef4861ade4a10a2d4fe26bc8
Kenneth Ekandem <kennyekandem@gmail.com>
2023-03-13 10:53:23 +0100

    update: learning integers

--- main.go
+++ main.go
@@ -6,5 +6,19 @@ import ("fmt")
 
 func main() {
 
+	var a int = 12
+	// c := "test"
+
+	var (
+		c string = "test"
+		name string = "Kenneth"
+		age int = 24
+		get_false bool = true
+	)
+
 	fmt.Println("Hello World")
+	fmt.Println(a)
+	fmt.Println(c)
+	fmt.Printf("%T, %v", c, c)
+	fmt.Println(c, name, age, get_false)
 }
